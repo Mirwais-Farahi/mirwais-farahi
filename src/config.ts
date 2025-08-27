432b1ae6bf1d756c8e9a0af819c9fa8d8e654a50
Mirwais Farahi <waisfarahi@example.com>
2025-08-27 11:53:28 +0430

    Add File

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Mirwais Farahi - Data Professional and System Analyst';
+export const SITE_DESCRIPTION = 'Data Professional and Systems Analyst with a proven record of delivering data-driven insights, revenue forecasting models, and
+aviation IT system solutions. Strong background in SQL, machine learning, and BI dashboards, with experience spanning civil aviation
+systems, government platforms, and large-scale international projects (UN FAO, USAID). Adept at building and optimizing commercial
+analytics systems that improve forecasting accuracy, reduce reporting turnaround, and support executive decision-making. Skilled
+communicator, team leader, and problem-solver with a reputation for resilience and accountability in high-pressure, multicultural
+environments.';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
